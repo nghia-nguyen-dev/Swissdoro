3e1d875a193a64b6c8b336497b7104b31060036a
Nghia <hello.nghia22@gmail.com>
2021-05-26 16:07:56 -0400

    Add logic

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { merge, pipe, compose } from "ramda";
 
-const startBtn = document.querySelector(".start-btn");
-const breakbtn = document.querySelector(".break-btn");
+const timer = document.querySelector(".timer");
+const counter = document.querySelector(".counter");
 const startBtnText = document.querySelector(".start-btn__text");
+const startBtn = document.querySelector(".start-btn");
+
 let intervalId: number;
 
-const SECONDS = 60;
-const STUDY = 25;
+const SECONDS = 3;
+const STUDY = 1;
 const BREAK = 5;
 const START = "start";
 const STOP = "stop";
@@ -19,26 +21,45 @@ let state = {
 	startTimer: false,
 };
 
-function countDown() {
-	if (startBtnText?.textContent === START) {
+const toggleText = textContent => startBtnText?.textContent = textContent === START ? STOP : START;
+
+const reset = () => {
+	clearInterval(intervalId);
+	updateState({time: STUDY * SECONDS });
+	console.log(state.time)
+	startBtnText?.textContent = 'reset'
+}
+
+const countDown = () => {
+	const txt = startBtnText?.textContent
+	if (txt === START) {
 		intervalId = setInterval(() => {
+			if (state.time <= 0) return reset();
+			toggleText(txt)
 			updateState({ time: state.time - 1, startTimer: true });
 			console.log(state.time);
 		}, 1000);
 	} else {
 		clearInterval(intervalId);
-		console.log(state);
+		console.log(state.time);
+		toggleText(txt);
 	}
 }
 
-const getMinutes = time => Math.floor(time / 60);
-const getSeconds = time => time % 60;
+startBtn?.addEventListener('click', countDown)
 
-const renderText = startTimer => (startTimer ? STOP : START);
+const getMinutes = time => Math.floor(time / 60) < 10 ? "0" + String(Math.floor(time / 60)) : Math.floor(time / 60);
+const getSeconds = time => time % 60 < 10 ? String(time % 60) + "0" : time % 60;
 
-const render = () => {};
+const render = state => {
+	timer?.textContent = getMinutes(state.time);
+	counter?.textContent = state.count
+	document.title = `Swissdoro | ${getMinutes(state.time)}:${getSeconds(
+		state.time
+	)}`;
+};
 
 const updateState = newState => {
-	state = merge(state, newState);
-	render(state);
-};
+	state = merge(state,newState);
+	render(state)
+}
